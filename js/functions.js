0b7b0bca0224f33a33b14f814d178ebbca749525
SlavChek <vs.litvinov@gmail.com>
2023-02-21 20:33:05 +0400

    Исправления с консультации

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,3 @@
-
-
 const counter = 10;
 const string = '-7 Лёша на полке клопа нашёл 7-';
 
@@ -12,16 +10,13 @@ checkLengthString(string, counter);
 // Функция для проверки, является ли строка палиндромом.
 
 const checkStringPalindrome = (str) => {
-  str = String(str).toLowerCase().replace(/\s/g, '');
+  str = str.toLowerCase().replace(/\s/g, '');
   return str === str.split('').reverse().join('');
 };
 
 checkStringPalindrome(string);
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
-
-// Не смог получить NaN в таком варианте решения возможно ли это??
-// const extractNumber = (str) => typeof str === 'number' ? parseInt(str.toString().replace('.', ''), 10) : Number(str.replace(/[/\D/]/gi, ''));
 
 const extractNumber = (str) => {
   if (typeof str === 'number') {
@@ -37,19 +32,6 @@ extractNumber(string);
 
 // Функция, которая принимает три параметра
 
-// решение от Академии
-const myPadStart = (str, minLength, pad) => {
+const addPadStart = (str, minLength, pad) => str.padStart(minLength, pad);
 
-  const actualPad = minLength - str.length;
-  if (actualPad <= 0) {
-    return str;
-  }
-  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + str;
-};
-
-myPadStart('q', 4, 'werty');
-
-// Мое решение
-// const myPadStart = (str, minLength, pad) => str.padStart(minLength, pad);
-
-export { checkLengthString, checkStringPalindrome, extractNumber, myPadStart };
+export { checkLengthString, checkStringPalindrome, extractNumber, addPadStart };
