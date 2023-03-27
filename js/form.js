98df7f14a36a70cfa138e90654486770fbdf741e
SlavChek <vs.litvinov@gmail.com>
2023-03-27 17:54:51 +0400

    fix: исправил ошибку

---
+++ js/form.js
@@ -0,0 +1,8 @@
+// Регулярное выражение для валидации хешьега
+const REGEXP_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
+// Максимальное колличество хештегов
+const MAX_HASHTAG_COUNTER = 5;
+
+// Находим форму
+document.querySelector('.');
+
