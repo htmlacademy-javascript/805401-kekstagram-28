44c171ba709eddcfb3f4b1d4d4eb3454db23bbd9
SlavChek <vs.litvinov@gmail.com>
2023-03-28 13:47:14 +0400

    feat: реализованна валидация формы через Pristine

--- js/form.js
+++ js/form.js
@@ -1,5 +1,10 @@
 import { isEscapeKeydown } from './util.js';
 
+// Регулярное выражение для проверки хэш-тегов
+const REGEXP_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
+// Максимальное количество хэш-тегов
+const HASHTAG_MAX_COUNT = 5;
+
 // Находим body
 const body = document.querySelector('body');
 // Находим форму
@@ -31,7 +36,7 @@ const onCloseImgUploadForm = () => {
   document.removeEventListener('keydown', onCloseImgUploadFormKeydown);
 };
 
-// Функция отменяет закрытие при активном поле хэштэга или комментария к фотографии
+// Функция отменяет закрытие при активном поле хэш-тега или комментария к фотографии
 
 const ifTheFieldInFocus = () => document.activeElement === hashtagInput || document.activeElement === commentField;
 
@@ -44,7 +49,67 @@ function onCloseImgUploadFormKeydown(evt) {
   }
 }
 
+// Валидация формы Pristine
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+});
+
+// Функция форматирует данные для проверки
+
+const formatStringToArray = (string) => {
+  const array = string.trim().split(' ').filter((arr) => arr.trim().length);
+  return array;
+};
+
+// Функция, проверяющая хэш-тег на соответствие с патерном
+
+const checkedValidationHashtag = (string) => {
+  const pattern = REGEXP_HASHTAG;
+  return pattern.test(string);
+};
+
+// Функция проверки валидности хэш-тегов
+
+const validationHashtag = (string) => {
+  const array = formatStringToArray(string);
+  return array.every(checkedValidationHashtag);
+};
+
+// Функция проверяет количество хэш-тегов
+
+const validationTheNumberOfHashtags = (value) => {
+  const array = formatStringToArray(value);
+  return array.length <= HASHTAG_MAX_COUNT;
+};
+
+// Функция проверяет одинвковые хэш-теги
+
+const validationDubleHashtags = (value) => {
+  const array = formatStringToArray(value);
+  return new Set(array).size === array.length;
+};
+
+// Создаём валидаторы
+
+pristine.addValidator(hashtagInput, validationHashtag, 'Неправильно введен #хэш-тег!');
+pristine.addValidator(hashtagInput, validationTheNumberOfHashtags, `Нельзя ввести больше ${HASHTAG_MAX_COUNT} #хэш-тегов`);
+pristine.addValidator(hashtagInput, validationDubleHashtags, 'Такой #хэш-тег уже существует, введите уникальный хештег');
+
+// Функция проверки валидации формы перед отправкой
+
+const onImgUploadForm = (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    imgUploadForm.submit();
+  }
+};
+
 // Открываем формы при выборе фото
 imgUploadFile.addEventListener('change', onOpenImgUploadForm);
 // Закрываем при нажатии на кнопку
 imgUploadCancel.addEventListener('click', onCloseImgUploadForm);
+// Отправка формы при валидном вводе данных
+imgUploadForm.addEventListener('submit', onImgUploadForm);
