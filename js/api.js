d4b889a3577fa30690b94d3945d080ad7a534b94
SlavChek <vs.litvinov@gmail.com>
2023-04-03 20:36:50 +0400

    feat: Реализована универсальная функция отправки получения данных, перечесления

--- js/api.js
+++ js/api.js
@@ -1,9 +1,54 @@
-import { renderGallery } from './gallery.js';
-const BASE_URL = 'https://28.javascript.pages.academy/kekstagram/data';
+// import { renderGallery } from './gallery.js';
 
-fetch(`${BASE_URL}`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderGallery(data);
-  });
+// Адрес получаемых данных
+const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
+
+// Заглушки для методов
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+// Методы
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+// Текст ошибок для методов
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
+
+// Функция получает и отправляет данные
+
+const load = (route, errorText, method = Method.GET, body = null) =>
+  fetch(`${BASE_URL}${route}`, { method, body })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .catch(() => {
+      throw new Error(errorText);
+    });
+
+// Функция получает данные
+
+const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
+
+// Функция отправляет данные
+
+const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+
+export { getData, sendData };
+
+// // Получаем и отрисовываем данные на странице
+// fetch(`${BASE_URL}`)
+//   .then((response) => response.json())
+//   .then((photo) => {
+//     renderGallery(photo);
+//   });
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // Импорты модулей
 
 // import { getRandomPhotoGallery } from './data.js';
-// import { renderGallery } from './gallery.js';
+import {getData} from './api.js';
+import { renderGallery } from './gallery.js';
 import './form.js';
 import './api.js';
 
 // renderGallery(getRandomPhotoGallery());
 
+getData()
+  .then((photos) => {
+    renderGallery(photos);
+  });
 
